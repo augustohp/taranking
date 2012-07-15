5bcaacb1595fdb3cafcdb8f6772916dedfe9c157
Augusto Pascutti <augusto@phpsp.org.br>
2012-07-15 06:16:49 -0300

    Created Ranking\Route\Routine\Auth to manage authentication.
    It actually does not any kind of Authentication, it just authorizes
    an user that is already logged in. This is used with Rest in order
    to make private URLs.

---
+++ src/Ranking/Route/Routine/Auth.php
@@ -0,0 +1,28 @@
+<?php
+namespace Ranking\Route\Routine;
+
+use \PHP_SESSION_ACTIVE;
+
+/**
+ * Authenticated routes routine.
+ *
+ * @author Augusto Pascutti <augusto@phpsp.org.br>
+ */
+class Auth
+{
+    public function __construct()
+    {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+    public function __invoke()
+    {
+        if (!isset($_SESSION['user']))
+            return true;
+
+        header('HTTP/1.1 403 You must be authenticated to access that URL');
+        header('Location: /');
+        return false;
+    }
+}
