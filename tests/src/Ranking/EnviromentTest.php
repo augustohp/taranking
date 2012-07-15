bb0f39b1bff1f794840f94e607821b181565a6e8
Augusto Pascutti <augusto@phpsp.org.br>
2012-07-15 09:26:24 -0300

    Improved Ranking\Enviroment test testing Exceptions

--- tests/src/Ranking/EnviromentTest.php
+++ tests/src/Ranking/EnviromentTest.php
@@ -26,4 +26,42 @@ class EnviromentTest extends PHPUnit_Framework_TestCase
         $object = new Enviroment();
         $this->assertEquals($value, $object->$method(), "$method returing something different from expected");
     }
+
+    public function testGetDatabaseEnviromentWithoutArguments()
+    {
+        foreach ($this->_enviromentVars() as $args) {
+            list($name, $value) = $args;
+            putenv("$name=$value");
+        }
+        $env = new Enviroment;
+        $obj = $env->getDatabase();
+        $this->assertInstanceOf('StdClass', $obj);
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testGetInvalidMethod()
+    {
+        $env = new Enviroment;
+        $env->NonEcsiste();
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetInvalidDatabaseEnviromentOption()
+    {
+        $env = new Enviroment;
+        $env->getDatabase('non ecsiste!');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetInvalidDatabaseEnviromentOptionFromMagicMethod()
+    {
+        $env = new Enviroment;
+        $env->getDatabaseNonEcsiste();
+    }
 }
