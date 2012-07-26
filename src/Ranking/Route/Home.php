fbdb5fe526d579947b8367d2c15ef1996aa00b53
Augusto Pascutti <augusto@phpsp.org.br>
2012-07-26 10:42:35 -0300

    Changed /users/<id> to /users/<username>.
    - Created tests
    - Changed link from user registration
    - Implemented changes to get information by user name instead of
      user id
    - In public/index.php
        - Added `use` to shorten class usage
        - Added user name validator to the user route /users/*
        - Changed routine to not add user information if it already set

--- src/Ranking/Route/Home.php
+++ src/Ranking/Route/Home.php
@@ -2,15 +2,41 @@
 namespace Ranking\Route;
 
 use Respect\Rest\Routable;
+use Respect\Config\Container;
+use Doctrine\ORM\EntityManager;
 
 class Home implements Routable
 {
-    public function get()
+    const HTTP_USER_NOT_FOUND = 'HTTP/1.1 404 User not found';
+    const HTTP_USER_CREATED = 'HTTP/1.1 201 User created';
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $em;
+
+    public function __construct(EntityManager $em=null)
+    {
+        if (is_null($em)) {
+            $container = new Container(RANKING_CONF.DS.'Doctrine.ini');
+            $em        = $container->entityManager;
+        }
+        $this->em = $em;
+    }
+
+    public function get($username)
     {
         $vars       = array('_view'=>'home.html');
         $registered = filter_input(INPUT_GET, 'registered');
+        $repo       = $this->em->getRepository('Ranking\Entity\User');
+        $criteria   = array('name'=>$username);
+        $user       = $repo->findOneBy($criteria);
+        if (empty($user)) {
+            header(self::HTTP_USER_NOT_FOUND);
+            return array('_view'=>'404.html', 'msg'=>'User not found');
+        }
+        $vars['user'] = $user;
         if ($registered) {
-            header('HTTP/1.1 201 User created');
+            header(self::HTTP_USER_CREATED);
             $vars['notice'] = 'User successfully registered!';
         }
         return $vars;
