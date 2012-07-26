4b542f97b0f7807e1bd25b83365e3f0a3c9f32fa
Augusto Pascutti <augusto@phpsp.org.br>
2012-07-26 05:46:06 -0300

    Created Ranking\Entity\Match test case in PHPUnit.
    A match is the object that holds information of when players have
    played, whom they played with, where (the map) they played.
    Currently I am creating a match with only one *creator*, but a
    game to be valid (in - I hope - a near future) should be created
    or corroborated/moderated by more than 50% of its players.

---
+++ tests/src/Ranking/Entity/MatchTest.php
@@ -0,0 +1,54 @@
+<?php
+use Ranking\Entity\Match;
+use Ranking\Entity\User;
+use Ranking\Entity\Map;
+
+class MatchTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Ranking\Entity\Match::getId
+     */
+    public function testGetId()
+    {
+        $id       = 1;
+        $match    = new Match();
+        $class    = new ReflectionClass($match);
+        $property = $class->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($match, $id);
+
+        $this->assertEquals($id, $match->getId());
+    }
+
+    public function _propertyAndValidValue() 
+    {
+        return array(
+            array('created', new DateTime),
+            array('played', new DateTime),
+            array('creator', new User),
+            array('map', new Map)
+        );
+    }
+
+    /**
+     * @dataProvider _propertyAndValidValue
+     */
+    public function testGetters($propertyName, $propertyValue)
+    {
+        $match    = new Match();
+        $class    = new ReflectionClass($match);
+        $property = $class->getProperty($propertyName);
+        $property->setAccessible(true);
+        $property->setValue($match, $propertyValue);
+
+        $methodName = 'get'.ucfirst(strtolower($propertyName));
+        $msgBase    = 'Method "%s" was expected to return "%s"';
+        if ($propertyValue instanceof DateTime) {
+            $propertyValueString = $propertyValue->format('Y-m-d');
+        } else {
+            $propertyValueString = (string) $propertyValue;
+        }
+        $msg        = sprintf($msgBase, $methodName, $propertyValueString);
+        $this->assertEquals($propertyValue, $match->$methodName(), $msg);
+    }
+}
