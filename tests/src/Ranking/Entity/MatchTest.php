3911c7c99eb65357786e451cf2be228e32a8e840
Augusto Pascutti <augusto@phpsp.org.br>
2012-07-26 06:24:17 -0300

    Created basic setters/getters for Ranking\Entity\Match
    - Basic entities cover these properties: id, played (datetime),
      created (datetime), creator (Ranking\Entity\User) and
      map (Ranking\Entity\Map)
    - Replaced the old getter test for more descriptive and simple
      solutions. Simple is always better for unit tests.
    
    No exceptions for validations were tested, this is pretty uggly.
    We are still missing the property that will hold the played of
    that given match in a soon commit. Please pandas, work, work and
    work! Or no bamboo for you today!

--- tests/src/Ranking/Entity/MatchTest.php
+++ tests/src/Ranking/Entity/MatchTest.php
@@ -5,6 +5,17 @@ use Ranking\Entity\Map;
 
 class MatchTest extends PHPUnit_Framework_TestCase
 {
+    protected $match, $user, $map;
+
+    public function setUp()
+    {
+        $this->match = new Match();
+        $this->user  = new User();
+        $this->map   = new Map();
+        $this->user->setName('testbot');
+        $this->map->setName('Test Dome');
+    }
+
     /**
      * @covers Ranking\Entity\Match::getId
      */
@@ -16,44 +27,112 @@ class MatchTest extends PHPUnit_Framework_TestCase
         $property = $class->getProperty('id');
         $property->setAccessible(true);
         $property->setValue($match, $id);
-
         $this->assertEquals($id, $match->getId());
     }
 
-    public function _propertyAndValidValue() 
+    /**
+     * @covers Ranking\Entity\Match::setCreator
+     */
+    public function testSetCreator()
     {
-        $user = new User();
-        $user->setName('testbot');
-        $map  = new Map();
-        $map->setName('Test Dome');
-
-        return array(
-            array('created', new DateTime),
-            array('played', new DateTime),
-            array('creator', $user),
-            array('map', $map)
-        );
+        $instance = $this->match->setCreator($this->user);
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeEquals($this->user, 'creator', $this->match);
+        return array($this->match, $this->user);
     }
 
     /**
-     * @dataProvider _propertyAndValidValue
+     * @depends testSetCreator
+     * @covers Ranking\Entity\Match::getCreator
      */
-    public function testGetters($propertyName, $propertyValue)
+    public function testGetCreator($args)
     {
-        $match    = new Match();
-        $class    = new ReflectionClass($match);
-        $property = $class->getProperty($propertyName);
-        $property->setAccessible(true);
-        $property->setValue($match, $propertyValue);
+        list($match, $creator) = $args;
+        $this->assertEquals($creator, $match->getCreator());
+    }
 
-        $methodName = 'get'.ucfirst(strtolower($propertyName));
-        $msgBase    = 'Method "%s" was expected to return "%s"';
-        if ($propertyValue instanceof DateTime) {
-            $propertyValueString = $propertyValue->format('Y-m-d');
-        } else {
-            $propertyValueString = (string) $propertyValue;
-        }
-        $msg        = sprintf($msgBase, $methodName, $propertyValueString);
-        $this->assertEquals($propertyValue, $match->$methodName(), $msg);
+    /**
+     * @covers Ranking\Entity\Match::setCreated
+     */
+    public function testSetCreatedWithoutAnyArgument()
+    {
+        $instance = $this->match->setCreated();
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeInstanceOf('DateTime', 'created', $this->match);
+    }
+
+    /**
+     * @covers Ranking\Entity\Match::setCreated
+     */
+    public function testSetCreatedWithDateTimeArgument()
+    {
+        $now = new DateTime();
+        $instance = $this->match->setCreated($now);
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeEquals($now, 'created', $this->match);
+        return array($this->match, $now);
+    }
+
+    /**
+     * @depends testSetCreatedWithDateTimeArgument
+     * @covers Ranking\Entity\Match::getCreated
+     */
+    public function testGetCreated($args)
+    {
+        list($match, $when) = $args;
+        $this->assertEquals($when, $match->getCreated());
+    }
+
+    /**
+     * @covers Ranking\Entity\Match::setPlayed
+     */
+    public function testSetPlayedWithoutAnyArgument()
+    {
+        $instance = $this->match->setPlayed();
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeInstanceOf('DateTime', 'played', $this->match);
+    }
+
+    /**
+     * @covers Ranking\Entity\Match::setPlayed
+     */
+    public function testSetPlayedWithDateTimeArgument()
+    {
+        $now = new DateTime();
+        $instance = $this->match->setPlayed($now);
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeEquals($now, 'played', $this->match);
+        return array($this->match, $now);
+    }
+
+    /**
+     * @depends testSetPlayedWithDateTimeArgument
+     * @covers Ranking\Entity\Match::getPlayed
+     */
+    public function testGetPlayed($args)
+    {
+        list($match, $when) = $args;
+        $this->assertEquals($when, $match->getPlayed());
+    }
+
+    /**
+     * @covers Ranking\Entity\Match::setMap
+     */
+    public function testSetMap()
+    {
+        $instance = $this->match->setMap($this->map);
+        $this->assertEquals($this->match, $instance);
+        $this->assertAttributeEquals($this->map, 'map', $this->match);
+        return array($this->match, $this->map);
+    }
+
+    /**
+     * @depends testSetMap
+     * @covers Ranking\Entity\Match::getMap
+     */
+    public function testGetMap($args)
+    {
+        list($match, $map) = $args;
+        $this->assertEquals($map, $match->getMap());
     }
 }
